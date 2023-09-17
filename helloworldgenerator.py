e13e67522aa44b2bb15ecca40743277c3f3e0695
Wolf <104569969+0x00wolf@users.noreply.github.com>
2023-09-17 01:48:41 -0300

    Update helloworldgenerator.py

--- helloworldgenerator.py
+++ helloworldgenerator.py
@@ -1,9 +1,6 @@
 file_bytesize = 512 * 1024
 message = "hello world"
-start = 0
-finish = 11010
-while start < finish:
+for i in range(100000)
     with open('test.txt', 'w') as f:
         repeat_amount = int((file_bytesize/len(message)))
         f.write(message*repeat_amount)
-    start += 1
