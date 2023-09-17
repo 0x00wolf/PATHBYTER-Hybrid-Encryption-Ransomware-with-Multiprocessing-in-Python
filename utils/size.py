6071c0a4ddd00d79be42efdeeb13c8c865988f8a
Wolf <104569969+0x00wolf@users.noreply.github.com>
2023-09-17 08:57:08 -0700

    Update size.py

--- utils/size.py
+++ utils/size.py
@@ -1,3 +1,7 @@
+"""This was the test I used to determine the size in bytes of encrypted key and nonce that would be appended to each file. 
+By determining the size I was able to concat the encoded JSON string to the encrypted file. When it came time to decrypt the file
+I could read the entire file into a variable, slice off the correct number of bytes from the end of the file to retreive the JSON string, 
+and create a new variable which was the file with the last 314 bytes removed."""
 from sys import getsizeof
 
 def get_size_str_in_bytes(string):
@@ -15,17 +19,3 @@ def sys_size_str_byres(string):
 stringi = b"{'k': 'DaYhWhEu2i1sF44CpBgDIEkA3AoFw20dJy20EwUw135sNj89c7LCli35yfUpoX3/7eDjZfF0TSSNSw2bkXAReu6H9UJDvRWWDryXI5W2+6ztti5/V1vtU2wzUiTASrqSXQLs5J0S99CLR/YLAEfrQzX3L9wVwl+GSu84LXJOqu5+Fop1lEV9+EUF0ibnk8n5idM7ukdBPCRk+z4r+sJLtu712nZk7XqMoZ+C9vhlrzwT+n9ykBS5P3q1IONth8gWjN1lrOndFgfCLHB2rdwhFqniJ2nmKVg3lou0RFQC4xNjaoe8RvnBdjGV0hnhLv4n1JyZf3EuB2tzkPeDjzSRvg==', 'n': 'wViSIgrx/ko='}"
 print(get_size_str_in_bytes(stringi.decode()))
 
-# ADD/REMOVE suffix from file name
-def add_file_suffix(target_files):
-    for _file in target_files:
-        if _file not in IGNORE_FILES:
-            rename(_file, f'{_file}.crypt')
-    
-    return
-
-
-def rm_file_suffix(target_files):
-    for _file in target_files:
-        if _file.endswith('.crypt'):
-            __file = _file.remove('.crypt')
-            rename(_file, __file)
